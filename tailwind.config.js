d229ae75f6898372f9a08625fde97f4665838daa
ABU_MUWAH <abu.muwah@gmail.com>
2022-08-31 20:48:59 +0430

    Adding profile section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,27 +7,29 @@ module.exports = {
       screens: {
         'xl': '1120px',
       },
-     },
+    },
+    // fontSize: {
+    //   '58px': '58px',
+    // },
     extend: {
-      // backgroundImage: {
-      //   '01': "url('/img/35.png')",
-      //   '02': "url('/img/36.png')",
-      //   '03': "url('/img/37.png')",
-      //   '04': "url('/img/38.png')",
-      // },
       height: {
         '0.5': '0.5px',
         '1px': '1px',
         '2px': '2px',
         '3px': '3px',
+        '158px': '158px',
+        '184px': '184px',
+        '208px': '208px',
         '500px': '500px',
-        '600px': '600px',
-        
+        '600px': '600px',    
       },
       width: {
         '0.5': '0.5px',
         '1px': '1px',
         '2px': '2px',
+        '158px': '158px',
+        '184px': '184px',
+        '208px': '208px',
         '374px': '374px',
         '500px': '500px',
         '265px': '265px',
